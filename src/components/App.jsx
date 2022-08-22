8f9fdc4e4e909ff7d4755764d252634b0d17b9e4
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-08-22 14:45:07 +0100

    initial components + routing done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Route, Switch } from 'react-router-dom';
+
+import { Actors, MovieInformation, Movies, NavBar, Profile } from '.';
+
+// -> root -> all movies
+// /asd123 -> movie information -> more
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <CssBaseline />
+      <NavBar />
+      <main>
+        <Switch>
+          <Route exact path="/">
+            <Movies />
+          </Route>
+          <Route exact path="/profile/:id">
+            <Profile />
+          </Route>
+          <Route exact path="/actors/:id">
+            <Actors />
+          </Route>
+          <Route exact path="/movie/:id">
+            <MovieInformation />
+          </Route>
+        </Switch>
+      </main>
+    </div>
+  );
 }
 
 export default App;
