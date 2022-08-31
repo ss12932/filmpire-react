62f012681ac86ce71fb219dca2273e8bbf9f4f70
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-09-01 00:09:20 +0100

    rendering movieslist + finishing view

---
+++ src/components/Movie/Movie.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Typography, Grid, Grow, Tooltip, Rating } from '@mui/material';
+import { Link } from 'react-router-dom';
+import useStyles from './styles';
+
+const Movie = ({ movie, i }) => {
+  const classes = useStyles();
+  console.log('Movie');
+  return (
+    <Grid item xs={12} sm={6} md={4} lg={3} xl={2} classes={classes.movie}>
+      <Grow in key={i} timeout={(i + 1) * 250}>
+        <Link className={classes.links} to={`/movie/${movie.id}`}>
+          <img
+            alt={movie.title}
+            className={classes.image}
+            src={
+              movie.poster_path
+                ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
+                : 'https://www.fillmurray.com/200/300'
+            }
+          />
+          <Typography className={classes.title} variant="h5">
+            {movie.title}
+          </Typography>
+        </Link>
+      </Grow>
+    </Grid>
+  );
+};
+
+export default Movie;
--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -6,15 +6,38 @@ import {
   Typography,
 } from '@mui/material';
 import { useSelector } from 'react-redux';
-
 import { useGetMoviesQuery } from '../../services/TMDB';
+import { MovieList } from '..';
 
 const Movies = () => {
-  const { data } = useGetMoviesQuery();
+  const { data, error, isFetching } = useGetMoviesQuery();
 
-  console.log(data);
-  console.log('Movies');
-  return <div>Movies</div>;
+  if (isFetching) {
+    return (
+      <Box display="flex" justifyContent="center">
+        <CircularProgress size="4rem" />
+      </Box>
+    );
+  }
+
+  if (!data.results.length) {
+    return (
+      <Box display="flex" alignItems="center" mt="20px">
+        <Typography variant="h4">
+          No movies that match that name.
+          <br />
+          Please search for something else.
+        </Typography>
+      </Box>
+    );
+  }
+
+  if (error) return 'An error has occured';
+  return (
+    <div>
+      <MovieList movies={data} />
+    </div>
+  );
 };
 
 export default Movies;
