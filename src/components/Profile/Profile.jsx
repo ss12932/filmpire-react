e4ccbc8c77dfe26a8c79462b715aa355728dd568
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-09-19 14:20:08 +0100

    authenticated profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+
+import { userSelector } from '../../features/auth';
 
 const Profile = () => {
-  console.log('Profile');
-  return <div>Profile</div>;
+  const { user } = useSelector(userSelector);
+  console.log(user);
+  return <div>Profile - {user.username}</div>;
 };
 
 export default Profile;
