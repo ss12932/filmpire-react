435a51fd02764f9fe8cbdd5d23e939c8670c89c6
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-09-20 22:51:23 +0100

    implement other voice commands for alan ai

--- src/components/Alan.jsx
+++ src/components/Alan.jsx
@@ -1,15 +1,38 @@
 import React, { useEffect, useContext } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import { fetchToken } from '../utils';
 import { ColorModeContext } from '../utils/ToggleColorMode';
+import {
+  selectGenreOrCategory,
+  searchMovie,
+} from '../features/currentGenreOrCategory';
 
 const useAlan = () => {
   const { setMode } = useContext(ColorModeContext);
+  const dispatch = useDispatch();
+  const history = useHistory();
+
   useEffect(() => {
     alanBtn({
-      key: '39d7fefddc1c227d1555c72afbeb30272e956eca572e1d8b807a3e2338fdd0dc/stage',
-      onCommand: ({ command, mode }) => {
-        if (command === 'changeMode') {
+      key: process.env.REACT_APP_ALAN_KEY,
+      onCommand: ({ command, mode, genres, genreOrCategory, query }) => {
+        if (command === 'chooseGenre') {
+          const foundGenre = genres.find(
+            (g) => g.name.toLowerCase() === genreOrCategory.toLowerCase(),
+          );
+          if (foundGenre) {
+            history.push('/');
+            dispatch(selectGenreOrCategory(foundGenre.id));
+          } else {
+            const category = genreOrCategory.startsWith('top')
+              ? 'top_rated'
+              : genreOrCategory;
+            history.push('/');
+            dispatch(selectGenreOrCategory(category));
+          }
+        } else if (command === 'changeMode') {
           if (mode === 'light') {
             setMode('light');
           } else {
@@ -19,7 +42,9 @@ const useAlan = () => {
           fetchToken();
         } else if (command === 'logout') {
           localStorage.clear();
-          window.location.href = '/';
+          history.push('/');
+        } else if (command === 'search') {
+          dispatch(searchMovie(query));
         }
       },
     });
